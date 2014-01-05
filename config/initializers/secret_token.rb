536c1c938dc871f318da6ba32b76971e2d401731
Mateo Acebedo <ma698@cornell.edu>
2014-01-04 20:41:54 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TimeSpendingTracker::Application.config.secret_token = '15398a660d8f4092fad4432c7a8610e4a167fe6b9ff1817218f41e524a64da7f214e550dc541816dd052c012e5aba86c8f4cf84e28b9351f67197f6485819e94'
